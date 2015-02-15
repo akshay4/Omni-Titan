5e1f5741d35d3870d6160904ef53a880fca9ae11
Akshay Singh Shekhawat <akshay.shekhawat4@gmail.com>
2015-02-15 17:02:56 +0530

    bootable

---
+++ BoardConfig.mk
@@ -0,0 +1,46 @@
+#
+# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# inherit from common msm8226-common
+-include device/motorola/msm8226-common/BoardConfigCommon.mk
+
+LOCAL_PATH := device/motorola/titan
+
+# Vendor Unification Init
+TARGET_UNIFIED_DEVICE := true
+TARGET_INIT_VENDOR_LIB := libinit_msm
+TARGET_LIBINIT_DEFINES_FILE := device/motorola/titan/init/init_titan.c
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := titan,titan_umts,titan_udstv,titan_umtsds,titan_retaildsds,XT1068,XT1064,XT1063,XT1069
+
+# Kernel
+BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/mkbootimg.mk
+TARGET_KERNEL_CONFIG := titan_defconfig
+
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE := 10444800
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 10526720
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1157627904
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 5930598400
+
+# Recovery
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/fstab.qcom
+
+# Device Resolution
+DEVICE_RESOLUTION := 720x1280
+
+# inherit from the proprietary version
+-include vendor/motorola/titan/BoardConfigVendor.mk
